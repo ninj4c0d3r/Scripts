d6e7c59bfe25a10f761446a8f86484e94e2b7590
Jonatas Fil <ctfninj4@gmail.com>
2016-07-29 19:39:04 -0300

    Create install.sh

---
+++ nmapwn/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+########################
+#    nmapwn v.01       #
+############################
+# Jonatas FIL a.k.a [Ninj4]#
+#################################
+# https://github.com/ninj4c0d3r #
+#################################
+# https://ninj4c0d3r.github.io  #
+#################################
+#  Install:                     #
+#  chmod +x install.sh          #
+# sudo ./install.sh             #
+#################################
+
+echo "aguarde até que a instalação termine."
+sleep 2
+
+mv modules/joomla-3_4_5.nse /usr/share/nmap/scripts/
+clear
+echo "Sucess !!!"
+sleep 1
+echo "chmod +x nmapwn.sh"
+echo "sudo ./nmapwn.sh"
+echo "enjoy :D"
